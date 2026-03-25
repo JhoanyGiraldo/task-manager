1dffafb37bca2aebe755a8b07bef7e4a3823aedc
JhoanyGiraldo <jhobani1992@gmail.com>
2026-03-25 16:15:15 -0500

    JWT funcionando con rutas protegidas (dashboard)

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { findUserByEmail } from "../models/userModel.js";
+import { createUser } from "../models/userModel.js";
 
 export const login = async (req, res) => {
   try {
@@ -18,9 +19,12 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "Contraseña incorrecta" });
     }
 
-    const token = jwt.sign(
-      { id: user.id, email: user.email },
-      "secreto_super_seguro",
+     const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
@@ -29,11 +33,27 @@ export const login = async (req, res) => {
       user: {
         id: user.id,
         name: user.name,
-        email: user.email,
-      },
+        email: user.email
+      }
     });
 
   } catch (error) {
     res.status(500).json({ message: "Error del servidor" });
+  }
+};
+
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await createUser(name, email, hashedPassword);
+
+    res.status(201).json(newUser);
+
+  } catch (error) {
+    res.status(500).json({ message: "Error al registrar usuario" });
   }
 };
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/register", register);
+
+router.get("/dashboard", verifyToken, (req, res) => {
+  res.json({
+    message: "Bienvenido al dashboard ",
+    user: req.user
+  });
+});
 
 export default router;
